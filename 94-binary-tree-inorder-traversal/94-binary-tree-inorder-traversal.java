a32b0eb30acebbc3bcc2f7650f21cc678c554a63
saumil11-coder <84261342+saumil11-coder@users.noreply.github.com>
2022-04-05 22:22:12 +0530

    Time: 2 ms (42.59%), Space: 42.4 MB (42.80%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -13,22 +13,29 @@
  *     }
  * }
  */
-class Solution { 
-    private void dfs(TreeNode node,List<Integer>inOrder)
-    {
-        if(node==null)
-        {
-            return;
-        }
-        dfs(node.left,inOrder);
-        inOrder.add(node.val);
-        dfs(node.right,inOrder);
-        
-    }
+class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer>inOrder=new ArrayList<>();
-        dfs(root,inOrder);
-        return inOrder;
-        
+       	List<Integer> ds=new ArrayList<>();
+				Stack<TreeNode> st=new Stack<>();
+				TreeNode node =root;
+			while(true){
+if(node!=null)
+{
+st.push(node);
+node=node.left;
+}
+else{
+if(st.isEmpty())
+break;
+else{
+node=st.pop();
+ds.add(node.val);
+node=node.right;
+}
+}
+
+
+}return ds;
+
     }
 }
