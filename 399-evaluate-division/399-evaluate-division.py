142fa453baca651f32d764d6ee8690138527a5f8
saumil11-coder <84261342+saumil11-coder@users.noreply.github.com>
2022-04-30 20:55:28 +0530

    Time: 30 ms (26.68%), Space: 13.3 MB (98.96%) - LeetHub

---
+++ 399-evaluate-division/399-evaluate-division.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def calcEquation(self, equations, values, queries):
+
+        graph = {}
+        
+        def build_graph(equations, values):
+            def add_edge(f, t, value):
+                if f in graph:
+                    graph[f].append((t, value))
+                else:
+                    graph[f] = [(t, value)]
+            
+            for vertices, value in zip(equations, values):
+                f, t = vertices
+                add_edge(f, t, value)
+                add_edge(t, f, 1/value)
+        
+        def find_path(query):
+            b, e = query
+            
+            if b not in graph or e not in graph:
+                return -1.0
+                
+            q = collections.deque([(b, 1.0)])
+            visited = set()
+            
+            while q:
+                front, cur_product = q.popleft()
+                if front == e:
+                    return cur_product
+                visited.add(front)
+                for neighbor, value in graph[front]:
+                    if neighbor not in visited:
+                        q.append((neighbor, cur_product*value))
+            
+            return -1.0
+        
+        build_graph(equations, values)
+        return [find_path(q) for q in queries]
