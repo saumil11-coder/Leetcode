5df52ba687a95fc1834f76d15d65159ff178dbc7
saumil11-coder <84261342+saumil11-coder@users.noreply.github.com>
2022-06-22 20:01:57 +0530

    Time: 1 ms (33.98%), Space: 42.9 MB (36.27%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,24 +1,34 @@
 class Solution {
+
     public boolean searchMatrix(int[][] matrix, int target) {
-        if(matrix.length==0) return false;
-       int n=matrix.length;
-int m=matrix[0].length;
-int left=0;
-int right=(n*m)-1;
-while(left<=right)
-{
-int mid=(left+(right-left)/2);
-if(matrix[mid/m][mid%m]==target){
-return true;}
-if(target>matrix[mid/m][mid%m])
-{
-left=mid+1;
-}
-else
-{
-right=mid-1;
-}
-}
-return false;
+        int selectRow = binarySearchSelectRow(matrix, target);
+        if (selectRow == -1) return false;
+        boolean numberFoundOrNot = numberPresentOrNot(matrix, selectRow, target);
+        return numberFoundOrNot;
+    }
+
+    public static int binarySearchSelectRow(int[][] matrix, int target) {
+        int low = 0;
+        int high = matrix.length - 1;
+        int lastColumn = matrix[0].length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (matrix[mid][0] <= target && target <= matrix[mid][lastColumn]) {
+                return mid;
+            } else if (matrix[mid][0] < target) low = mid + 1; else if (matrix[mid][0] > target) high = mid - 1;
+        }
+        return -1;
+    }
+
+    public static boolean numberPresentOrNot(int[][] matrix, int selectRow, int target) {
+        int low = 0;
+        int high = matrix[0].length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (matrix[selectRow][mid] == target) return true; else if (matrix[selectRow][mid] < target) {
+                low = mid + 1;
+            } else high = mid - 1;
+        }
+        return false;
     }
 }
