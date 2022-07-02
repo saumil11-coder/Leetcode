a272c117ace08da7e66189c095a53c17aaf4b19b
saumil11-coder <84261342+saumil11-coder@users.noreply.github.com>
2022-07-02 09:36:59 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (93.00%) - LeetHub

--- 100-same-tree/100-same-tree.java
+++ 100-same-tree/100-same-tree.java
@@ -14,9 +14,14 @@
  * }
  */
 class Solution {
- public boolean isSameTree(TreeNode p, TreeNode q) {
-if(p==null||q==null) return(p==q);
-return (p.val==q.val)&&isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if((p==null)||q==null)
+        {
+           return p==q ;
+        }
+        return( (p.val==q.val)
+            && isSameTree(p.left,q.left)
+            && isSameTree(p.right,q.right));
         
- }
+    }
 }
