a5522e3697e31c31b5e19c802736a10054069356
saumil11-coder <84261342+saumil11-coder@users.noreply.github.com>
2022-07-06 23:32:22 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (22.59%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,7 +1,11 @@
 class Solution {
+    int dp[]=new int[32];
     public int fib(int n) {
         if(n<=1)return n;
-        return fib(n-1)+fib(n-2);
+        if(dp[n]>0)
+            return dp[n];
+        dp[n]=fib(n-1)+fib(n-2);
+        return dp[n];
         
     }
 }
