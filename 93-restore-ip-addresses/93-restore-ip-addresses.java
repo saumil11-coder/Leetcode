5ce4546d4be241e8ffb531e5ceacd2c7d2fcb644
saumil11-coder <84261342+saumil11-coder@users.noreply.github.com>
2022-07-10 16:39:50 +0530

    Time: 9 ms (46.18%), Space: 42.7 MB (78.43%) - LeetHub

---
+++ 93-restore-ip-addresses/93-restore-ip-addresses.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<String> restoreIpAddresses(String s) {
+        List<String> ans=new ArrayList<>();
+        helper(s,0,0,"",ans);
+        return ans;
+    }
+    public void helper(String s,int i,int par,String ans,List<String>res)
+    {
+        if(i==s.length()||par==4)
+        {
+            if(s.length()==i &&par==4)
+            {
+                res.add(ans.substring(0,ans.length()-1));
+                
+            }
+            return;
+        }
+        helper(s,i+1,par+1,ans+s.charAt(i)+".",res);
+        if(i+2<=s.length()&& isValidRange(s.substring(i,i+2)))
+            helper(s,i+2,par+1,ans+s.substring(i,i+2)+".",res);
+        if(i+3<=s.length()&& isValidRange(s.substring(i,i+3)))
+            helper(s,i+3,par+1,ans+s.substring(i,i+3)+".",res);
+    }
+    public boolean isValidRange(String s)
+    {
+        if(s.charAt(0)=='0'){
+            return false;}
+        int val=Integer.parseInt(s);
+        return val<=255;
+    }
+}
