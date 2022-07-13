d137c719fb09b06ee06925b99d848ed5597c7c0c
saumil11-coder <84261342+saumil11-coder@users.noreply.github.com>
2022-07-13 09:08:46 +0530

    Time: 1 ms (92.53%), Space: 44.2 MB (6.93%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
@@ -14,27 +14,28 @@
  * }
  */
 class Solution {
-public List<List<Integer>> levelOrder(TreeNode root) {
-Queue<TreeNode> queue=new LinkedList<>();
-List<List<Integer>> bigList=new ArrayList<>();
-if(root==null) return bigList;
-queue.offer(root);
-while(!queue.isEmpty())
-{
-    int levelNum=queue.size();
-List<Integer> subList=new ArrayList<Integer>();
-for(int i=0;i<levelNum;i++)
-{
-if(queue.peek().left!=null) queue.offer(queue.peek().left);
-if(queue.peek().right!=null) queue.offer(queue.peek().right);
-subList.add(queue.poll().val);
-}
-bigList.add(subList);
-
-}
-return bigList;
-
-       
-}
-    
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> BigList=new ArrayList<>();
+        Queue<TreeNode> queue=new LinkedList<>();
+        if(root==null)
+        {
+            return BigList;
+        }
+        queue.offer(root);
+        while(!queue.isEmpty())
+        {
+            int levelNum=queue.size();
+            List<Integer> subList=new ArrayList<>();
+            for(int i=0;i<levelNum;i++)
+            {
+                if(queue.peek().left!=null)queue.offer(queue.peek().left);
+                if(queue.peek().right!=null)queue.offer(queue.peek().right);
+                subList.add(queue.poll().val);
+                
+            }
+            BigList.add(subList);
+        }
+        return BigList;
+        
     }
+}
