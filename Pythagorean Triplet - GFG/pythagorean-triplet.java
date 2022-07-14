5d30f635c7ee699cc20bc8b942014f8dfde07200
saumil11-coder <84261342+saumil11-coder@users.noreply.github.com>
2022-07-14 16:23:19 +0530

    Added solution - LeetHub

---
+++ Pythagorean Triplet - GFG/pythagorean-triplet.java
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[n];
+            String[] inputLine = br.readLine().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            boolean ans = new Solution().checkTriplet(arr, n);
+            System.out.println(ans ? "Yes" : "No");
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution {
+    boolean checkTriplet(int[] arr, int n) {
+    Arrays.sort(arr);
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            for(int k=j+1;k<n;k++)
+            {
+                if((arr[i]*arr[i])+(arr[j]*arr[j])==(arr[k]*arr[k]) )
+                {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+    
+        }
+}
