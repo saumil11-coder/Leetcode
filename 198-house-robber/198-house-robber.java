960b3bff0b94efb495dfbe2195057288129785db
saumil11-coder <84261342+saumil11-coder@users.noreply.github.com>
2022-07-17 10:23:56 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (87.21%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,19 +1,40 @@
+// class Solution {// memoization 
+//     public int helper(int idx,int nums[],int dp[])
+//     {
+//         if(idx==0)return nums[idx];
+//         if(idx<0)return 0;
+//         if(dp[idx]!=-1){return dp[idx];}
+//         int pick=nums[idx]+helper(idx-2,nums,dp);
+//         int notPick=0+helper(idx-1,nums,dp);
+//         return dp[idx]=Math.max(pick,notPick);
+//     }
+//     public int rob(int[] nums) {
+       
+//         int n=nums.length;
+//          int dp[]=new int[n];
+//         Arrays.fill(dp,-1);
+//         return helper(n-1,nums,dp);
+        
+//     }
+// }
 class Solution {
-    public int helper(int idx,int nums[],int dp[])
-    {
-        if(idx==0)return nums[idx];
-        if(idx<0)return 0;
-        if(dp[idx]!=-1){return dp[idx];}
-        int pick=nums[idx]+helper(idx-2,nums,dp);
-        int notPick=0+helper(idx-1,nums,dp);
-        return dp[idx]=Math.max(pick,notPick);
-    }
+  
     public int rob(int[] nums) {
        
         int n=nums.length;
-         int dp[]=new int[n];
-        Arrays.fill(dp,-1);
-        return helper(n-1,nums,dp);
+        int dp[]=new int[n+1];
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++)
+        {
+            int take=nums[i];
+            if(i>1)
+                take+=dp[i-2];
+            int notTake=0+dp[i-1];
+            dp[i]=Math.max(take,notTake);
+        }
+        return dp[n-1];
+        
+        
         
     }
 }
