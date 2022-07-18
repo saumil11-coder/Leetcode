577be73785c6221f5447b03266aca02b4c45d0a9
saumil11-coder <84261342+saumil11-coder@users.noreply.github.com>
2022-07-18 14:54:13 +0530

    Time: 3 ms (80.47%), Space: 47.2 MB (54.35%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.java
@@ -15,41 +15,43 @@
  */
 class Solution {
     private TreeNode first;
-private TreeNode middle;
-private TreeNode last;
-private TreeNode prev;
- private void inorder(TreeNode root)
-{
-if(root==null) return ;
-inorder(root.left);
-if(prev!=null&&(root.val<prev.val))
-{
-if(first==null)
-{
-first =prev;
-middle=root;
-}
-else last=root;
-}
-prev=root;
-inorder(root.right);
-
-}public void recoverTree(TreeNode root) {
-first=middle=last=null;
-prev=new TreeNode(Integer.MIN_VALUE);
-inorder(root);
-if(first!=null&&last!=null)
-{
-int temp=first.val;
-first.val=last.val;
-last.val=temp;
-}
- else if(first!=null&&middle!=null)
-{
-int temp=first.val;
-first.val=middle.val;
-middle.val=temp;
-}
-
-}
+    private TreeNode middle;
+    private TreeNode prev;
+    private TreeNode last;
+    public void helper(TreeNode root)
+    {
+        if(root==null)return ;
+        helper(root.left);
+        if(prev!=null && (root.val<prev.val))
+        {
+            if(first==null)
+            {
+                first=prev;
+                middle=root;
+            }
+        
+        else{
+            last=root;
+        }
+        }
+        prev =root;
+        helper(root.right);
+    }
+    public void recoverTree(TreeNode root) {
+        first=last=middle=null;
+        prev=new TreeNode(Integer.MIN_VALUE);
+        helper(root);
+        if(first!=null && last!=null)
+        {
+            int temp=first.val;
+            first.val=last.val;
+            last.val=temp;
+        }
+        else if(first!=null && middle!=null)
+        {
+            int n=first.val;
+            first.val=middle.val;
+            middle.val=n;
+        }
+    }
 }
