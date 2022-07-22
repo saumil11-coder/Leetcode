f3c44dafc57d3ef44600167d053696fdcfd6fcdb
saumil11-coder <84261342+saumil11-coder@users.noreply.github.com>
2022-07-22 11:13:31 +0530

    Time: 1 ms (79.72%), Space: 45.2 MB (64.54%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
@@ -14,24 +14,22 @@
  * }
  */
 class Solution {
-  int i=1;//keep track
-int ans=0;
-public int kthSmallest(TreeNode root, int k) {
-
-inorder(root,k);
-return ans;
+    int ans=0;
+    int i=1;
+    public int kthSmallest(TreeNode root, int k) {
+        inorder(root,k);
+        return ans;
         
-}
-public void inorder(TreeNode node,int k)
-{
-if(node==null) return ;
-inorder(node.left,k);
-if(i==k)
-{
-ans=node.val;
-}
-i++;
-inorder(node.right,k);
-}
-
+    }
+    public void inorder(TreeNode root,int k)
+    {
+        if(root==null) return ;
+        inorder(root.left,k);
+        if(i==k)
+        {
+            ans=root.val;
+        }
+        i++;
+        inorder(root.right,k);
+    }
 }
