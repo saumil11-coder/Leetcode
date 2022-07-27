0dc9753aeb93a6bcf69dfc54f7f4f9c30a503e5b
saumil11-coder <84261342+saumil11-coder@users.noreply.github.com>
2022-07-27 16:17:59 +0530

    Time: 212 ms (21.80%), Space: 69.1 MB (12.11%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -21,39 +21,65 @@
 
 
 
+// class Solution {
+//     public boolean canJump(int[] nums) {
+//         int dp[]=new int[nums.length+1];
+//         Arrays.fill(dp,-1);
+//         return helper(0,nums,dp);
+        
+//     }
+//     public boolean helper(int idx,int nums[],int dp[])
+//     {
+//         if(idx==nums.length-1)
+//         {
+//             dp[idx]=1;
+//             return true;
+//         }
+//         if(idx>nums.length-1) 
+//         {
+//             dp[idx]=0;
+//             return false;
+//         }
+//             if(dp[idx]==0)   return false;
+//         if(dp[idx]==1) return true;
+    
+//         for(int j =1;j<=nums[idx];j++)
+//         {
+//             if(helper(idx+j,nums,dp)){
+//                 dp[idx]=1;
+//                 return true;}
+            
+//         }
+//         dp[idx]=0;
+//         return false;
+//     }
+// }
+
+
+
 class Solution {
+    
     public boolean canJump(int[] nums) {
-        int dp[]=new int[nums.length+1];
-        Arrays.fill(dp,-1);
-        return helper(0,nums,dp);
+        int n=nums.length-1;
+    
+        int dp[]=new int[n+1];
+        dp[n]=1;
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=1;j<=nums[i];j++)
+            {
+                if(dp[i+j]==1)
+                {
+                    dp[i]=1;
+                    break;
+                }
+            }
+        }
+        return dp[0]==0?false:true;
         
     }
-    public boolean helper(int idx,int nums[],int dp[])
-    {
-        if(idx==nums.length-1)
-        {
-            dp[idx]=1;
-            return true;
-        }
-        if(idx>nums.length-1) 
-        {
-            dp[idx]=0;
-            return false;
-        }
-            if(dp[idx]==0)   return false;
-        if(dp[idx]==1) return true;
-    
-        for(int j =1;j<=nums[idx];j++)
-        {
-            if(helper(idx+j,nums,dp)){
-                dp[idx]=1;
-                return true;}
-            
-        }
-        dp[idx]=0;
-        return false;
-    }
 }
+
 
 
 
