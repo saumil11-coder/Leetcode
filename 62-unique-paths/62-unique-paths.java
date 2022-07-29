b74b2edf73df7ca5233c648817fe0e30124cf34d
saumil11-coder <84261342+saumil11-coder@users.noreply.github.com>
2022-07-29 12:42:56 +0530

    Time: 1 ms (53.06%), Space: 40.7 MB (69.71%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,23 +1,35 @@
 class Solution {
-    public int helper(int i,int j,int dp[][])
-    {
-        //base case
-        if(i==0 && j==0)return 1;
-        if(i<0 || j<0) return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        //logic
-        int up=helper(i-1,j,dp);
-        int left=helper(i,j-1,dp);
-        return dp[i][j]=up+left;
+//     public int helper(int i,int j,int dp[][])
+//     {
+//         //base case
+//         if(i==0 && j==0)return 1;
+//         if(i<0 || j<0) return 0;
+//         if(dp[i][j]!=-1)return dp[i][j];
+//         //logic
+//         int up=helper(i-1,j,dp);
+//         int left=helper(i,j-1,dp);
+//         return dp[i][j]=up+left;
         
-    }
+//     }
     public int uniquePaths(int m, int n) {
+       //tabulation 
         int dp[][]=new int[m][n];
-        for(int []rows:dp)
+        for(int i=0;i<m;i++)
         {
-            Arrays.fill(rows,-1);
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 && j==0){ dp[i][j]=1; 
+                                 continue;}
+                else{
+                    int up=0;
+                    int left=0;
+                    if(i>0) up=dp[i-1][j];
+                    if(j>0)left=dp[i][j-1];
+                    dp[i][j]=up+left;
+                }
+            }
         }
-        return helper(m-1,n-1,dp);
+        return dp[m-1][n-1];
         
     }
 }
