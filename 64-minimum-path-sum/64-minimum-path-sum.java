bc9cb62788fe45a283fcbba9ae6d7bd6d84ce3b3
saumil11-coder <84261342+saumil11-coder@users.noreply.github.com>
2022-08-02 12:10:06 +0530

    Time: 6 ms (11.93%), Space: 46.1 MB (35.28%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -1,21 +1,35 @@
 class Solution {
-    public int helper(int i,int j,int grid[][],int dp[][])
-    {
-        if(i==0 && j==0 ) return grid[0][0];
-        if(i<0 || j<0)return (int)Math.pow(10,9);
-        if(dp[i][j]!=-1) return dp[i][j];
-        int up=grid[i][j]+helper(i-1,j,grid,dp);
-        int left=grid[i][j]+helper(i,j-1,grid,dp);
-        return dp[i][j]=Math.min(up,left);
-    }
+    // public int helper(int i,int j,int grid[][],int dp[][])
+    // { //memoization 
+    //     if(i==0 && j==0 ) return grid[0][0];
+    //     if(i<0 || j<0)return (int)Math.pow(10,9);
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     int up=grid[i][j]+helper(i-1,j,grid,dp);
+    //     int left=grid[i][j]+helper(i,j-1,grid,dp);
+    //     return dp[i][j]=Math.min(up,left);
+    // }
     public int minPathSum(int[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
         int dp[][]=new int[m][n];
-        for(int row[]:dp)
+        for(int i=0;i<m;i++)
         {
-            Arrays.fill(row,-1);
+            for(int j=0;j<n;j++)
+            {
+                 if(i==0 && j==0 )dp[i][j]=grid[i][j];
+                else{
+                    int up=0; int left=0;
+                 if(i>0) up =grid[i][j]+dp[i-1][j];
+                    else   up=grid[i][j]+ (int)Math.pow(10,9);
+            
+                 if(j>0) left= grid[i][j]+ dp[i][j-1];
+                    else left=grid[i][j]+ (int)Math.pow(10,9);
+                    
+                      dp[i][j]=Math.min(up,left);
+                }
+              
+            }
         }
-        return helper(m-1,n-1,grid,dp);
-    }
+        return dp[m-1][n-1];
+        }
 }
