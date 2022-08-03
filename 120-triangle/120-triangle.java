bb173d1132931543fa8d6f233e8b0909adb30d7d
saumil11-coder <84261342+saumil11-coder@users.noreply.github.com>
2022-08-03 09:30:19 +0530

    Time: 5 ms (53.22%), Space: 43.5 MB (79.53%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,6 +1,6 @@
 class Solution {
     public int helper(int i,int j,List<List<Integer>> lst,int n,int dp[][])
-    {
+    {// memoization 
         if(i==n-1)return lst.get(n-1).get(j);
         if(dp[i][j]!=-1) return dp[i][j];
         int down=lst.get(i).get(j)+helper(i+1,j,lst,n,dp);
@@ -10,8 +10,19 @@ class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n=triangle.size();
         int [][]dp=new int[n][n];
-        for(int row[]:dp)
-        Arrays.fill(row,-1);
-        return helper(0,0,triangle,n,dp);
+        for(int j=0;j<n;j++)
+        {
+            dp[n-1][j]=triangle.get(n-1).get(j);
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j=i;j>=0;j--)
+            {
+                int bottom =triangle.get(i).get(j)+ dp[i+1][j];
+                int diagonal =triangle.get(i).get(j)+ dp[i+1][j+1];
+                dp[i][j]=Math.min(bottom,diagonal);
+            }
+        }
+        return dp[0][0];
     }
 }
