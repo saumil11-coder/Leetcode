59f5268276c3f56f9d02c5eb1e65790a7464ef11
saumil11-coder <84261342+saumil11-coder@users.noreply.github.com>
2022-08-03 09:19:07 +0530

    Time: 2 ms (94.74%), Space: 44 MB (56.45%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,11 +1,17 @@
 class Solution {
- public int minimumTotal(List<List<Integer>> triangle) {
-    int[] A = new int[triangle.size()+1];
-    for(int i=triangle.size()-1;i>=0;i--){
-        for(int j=0;j<triangle.get(i).size();j++){
-            A[j] = Math.min(A[j],A[j+1])+triangle.get(i).get(j);
-        }
+    public int helper(int i,int j,List<List<Integer>> lst,int n,int dp[][])
+    {
+        if(i==n-1)return lst.get(n-1).get(j);
+        if(dp[i][j]!=-1) return dp[i][j];
+        int down=lst.get(i).get(j)+helper(i+1,j,lst,n,dp);
+        int diagonal=lst.get(i).get(j)+helper(i+1,j+1,lst,n,dp);
+        return dp[i][j]= Math.min(down,diagonal);
     }
-    return A[0];
-}
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n=triangle.size();
+        int [][]dp=new int[n][n];
+        for(int row[]:dp)
+        Arrays.fill(row,-1);
+        return helper(0,0,triangle,n,dp);
+    }
 }
