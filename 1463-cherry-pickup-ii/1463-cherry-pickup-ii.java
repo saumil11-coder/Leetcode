caab090ad72a170434e7639e645542c4db4ec70c
saumil11-coder <84261342+saumil11-coder@users.noreply.github.com>
2022-08-05 11:26:31 +0530

    Time: 107 ms (9.49%), Space: 48.5 MB (49.60%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -24,14 +24,14 @@ class Solution {
     //     return dp[i][j1][j2] = maxi;
     // }
 
-    public int cherryPickup(int[][] grid) {
+    public int cherryPickup(int[][] grid) { // tabulation 
         int n = grid.length;
         int m = grid[0].length;
-        int dp[][][] = new int[n][m][m];
-
+        int front[][]=new int[m][m];
+        int curr[][]=new int[m][m];
         for (int j1 = 0; j1 < m; j1++) {
             for (int j2 = 0; j2 < m; j2++) {
-                if (j1 == j2) dp[n - 1][j1][j2] = grid[n - 1][j1]; else dp[n - 1][j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
+                if (j1 == j2) front[j1][j2] = grid[n - 1][j1]; else front[j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
             }
         }
 
@@ -45,16 +45,20 @@ class Solution {
 
                             if (j1 == j2) ans = grid[i][j1]; else ans = grid[i][j1] + grid[i][j2];
 
-                            if ((j1 + di < 0 || j1 + di >= m) || (j2 + dj < 0 || j2 + dj >= m)) ans += (int) Math.pow(-10, 9); else ans += dp[i + 1][j1 + di][j2 + dj];
+                            if ((j1 + di < 0 || j1 + di >= m) || (j2 + dj < 0 || j2 + dj >= m)) ans += (int) Math.pow(-10, 9); else ans += front[j1 + di][j2 + dj];
 
                             maxi = Math.max(ans, maxi);
                         }
                     }
-                    dp[i][j1][j2] = maxi;
+                    curr[j1][j2] = maxi;
                 }
+            }
+            for(int a=0;a<m;a++)
+            {
+                front[a]=(int[])(curr[a].clone());
             }
         }
 
-        return dp[0][0][m - 1];
+        return front[0][m - 1];
     }
 }
