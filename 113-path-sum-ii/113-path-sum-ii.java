fe37c22b7de7f1913eca3f285473fb5f90d6ed2f
saumil11-coder <84261342+saumil11-coder@users.noreply.github.com>
2022-09-24 21:14:31 +0530

    Time: 2 ms (85.41%), Space: 45.2 MB (20.30%) - LeetHub

---
+++ 113-path-sum-ii/113-path-sum-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<Integer> currentPath = new ArrayList();
+        List<List<Integer>> allPaths = new ArrayList();
+        findPathsRecursive(root,targetSum,currentPath,allPaths);
+        return allPaths;
+    }
+    private static void findPathsRecursive(TreeNode currentNode, int sum, List<Integer> currentPath,List<List<Integer>> allPaths){
+         if(currentNode==null)return;
+         
+         currentPath.add(currentNode.val);
+        
+         if(currentNode.val==sum && currentNode.left==null && currentNode.right==null)
+             allPaths.add(new ArrayList<Integer>(currentPath));
+         else{
+            
+             findPathsRecursive(currentNode.left,sum-currentNode.val,currentPath,allPaths);
+             
+             findPathsRecursive(currentNode.right,sum-currentNode.val,currentPath,allPaths);
+         }
+       
+        
+         currentPath.remove(currentPath.size()-1);
+      }
+
+}
