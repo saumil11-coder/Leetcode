330ffd19aa2e226143bf47983a22c20ed54e627b
saumil11-coder <84261342+saumil11-coder@users.noreply.github.com>
2022-09-25 22:36:57 +0530

    Time: 11 ms (15.69%), Space: 48.2 MB (49.63%) - LeetHub

---
+++ 622-design-circular-queue/622-design-circular-queue.java
@@ -0,0 +1,64 @@
+class MyCircularQueue {
+
+    private int front;
+    private int rear;
+    private int[] arr;
+    private int cap;
+    
+    private int next(int i){ // to get next idx after i in circular queue
+        return (i+1)%cap;
+    }
+    private int prev(int i){ // to get prev idx before i in circular queue
+        return (i+cap-1)%cap;
+    }
+    
+	// rest is as simple as implmenting a normal queue using array.
+    public MyCircularQueue(int k) {
+        arr = new int[k];
+        cap=k;
+        front=-1;
+        rear=-1;
+    }
+    
+    public boolean enQueue(int value) {
+        if(isFull())return false;
+        if(front==-1){
+            front=0;
+            rear=0;
+            arr[rear]=value;
+            return true;
+        }
+        rear = next(rear);
+        arr[rear]=value;
+        return true;
+    }
+    
+    public boolean deQueue() {
+        if(isEmpty())return false;
+        if(front==rear){
+            front=-1;
+            rear=-1;
+            return true;
+        }
+        front=next(front);
+        return true;
+    }
+    
+    public int Front() {
+        if(front==-1)return -1;
+        return arr[front];
+    }
+    
+    public int Rear() {
+        if(rear==-1)return -1;
+        return arr[rear];
+    }
+    
+    public boolean isEmpty() {
+        return front==-1;
+    }
+    
+    public boolean isFull() {
+        return front!=-1 && next(rear)==front;
+    }
+}
