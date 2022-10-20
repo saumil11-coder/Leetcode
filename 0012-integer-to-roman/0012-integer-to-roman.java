b30f5a0f1f25e89af8ba67431892d2f092c03009
saumil11-coder <84261342+saumil11-coder@users.noreply.github.com>
2022-10-20 20:32:29 +0530

    Time: 18 ms (26.60%), Space: 49.4 MB (5.96%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String intToRoman(int num) {
+        var map=new LinkedHashMap<Integer,String>(); 
+map.put(1000, "M");
+        map.put(900, "CM");
+        map.put(500, "D");
+        map.put(400, "CD");
+        map.put(100, "C");
+        map.put(90, "XC");
+        map.put(50, "L");
+        map.put(40, "XL");
+        map.put(10, "X");
+        map.put(9, "IX");
+        map.put(5, "V");
+        map.put(4, "IV");
+        map.put(1, "I");
+StringBuilder sb=new StringBuilder();
+for(Map.Entry<Integer,String>el:map.entrySet())
+{
+while(num>=el.getKey())
+{
+num-=el.getKey();
+sb.append(el.getValue());
+}
+}
+return sb.toString();
+    }
+}
