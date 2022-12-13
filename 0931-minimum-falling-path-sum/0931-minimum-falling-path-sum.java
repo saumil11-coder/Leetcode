cec64d84937db017780c5c267f9a1cd5cf69c68b
saumil11-coder <84261342+saumil11-coder@users.noreply.github.com>
2022-12-13 21:24:39 +0530

    Time: 10 ms (43.15%), Space: 47.8 MB (35.59%) - LeetHub

---
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -0,0 +1,88 @@
+class Solution {
+    // public int helper(int i,int j,int n,int [][]matrix,int dp[][])
+    // {//memo
+    //     if(j<0 || j>=n) return (int)Math.pow(10,9);//for  preventing out of bound 
+    //     if(i==0)return matrix[i][j];// once we reach the 1st row we will return element\
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     int up=matrix[i][j]+helper(i-1,j,n,matrix,dp);
+    //     int upleft=matrix[i][j]+helper(i-1,j-1,n,matrix,dp);
+    //     int upright=matrix[i][j]+helper(i-1,j+1,n,matrix,dp);
+    //     return dp[i][j]=Math.min(up,Math.min(upleft,upright));
+    // }
+    
+    
+//     public int minFallingPathSum(int[][] matrix) {
+//      int  n=matrix.length;//tabulation
+//         int m=matrix[0].length;
+//         int dp[][]=new int[n][m];
+        
+//   for(int j=0;j<m;j++)
+//   {
+      
+//       dp[0][j]=matrix[0][j];
+//   }
+//         for(int i=1;i<n;i++)
+//         {
+//             for(int j=0;j<m;j++)
+//             {
+//                 int up=0,left=0,right=0;
+//                  up=matrix[i][j]+dp[i-1][j];
+//              if(j-1>=0)    left=matrix[i][j]+dp[i-1][j-1];
+//                 else left= matrix[i][j]+(int)Math.pow(10,9);
+
+//                 if(j+1<m)  right=matrix[i][j]+dp[i-1][j+1];
+//                 else right=matrix[i][j]+(int)Math.pow(10,9);
+//                  dp[i][j]=Math.min(up,Math.min(left,right));
+//                 }
+           
+//         }
+        
+//         int mini=Integer.MAX_VALUE;
+//         for(int j=0;j<m;j++)
+//         {
+//             mini=Math.min(mini,dp[n-1][j]);
+//         }
+//         return mini;
+//     }
+// }
+
+
+
+    public int minFallingPathSum(int[][] matrix) {
+     int  n=matrix.length;
+        int m=matrix[0].length;
+    
+        int prev[]=new int[m];
+       
+        
+  for(int j=0;j<m;j++)
+  {
+      
+      prev[j]=matrix[0][j];
+  }
+        for(int i=1;i<n;i++)
+        {
+             int curr[]=new int[m];
+            for(int j=0;j<m;j++)
+            {
+                int up=0,left=0,right=0;
+                 up=matrix[i][j]+prev[j];
+             if(j-1>=0)    left=matrix[i][j]+prev[j-1];
+                else left= matrix[i][j]+(int)Math.pow(10,9);
+
+                if(j+1<m)  right=matrix[i][j]+prev[j+1];
+                else right=matrix[i][j]+(int)Math.pow(10,9);
+                 curr[j]=Math.min(up,Math.min(left,right));
+                }
+            prev=curr;
+           
+        }
+        
+        int mini=Integer.MAX_VALUE;
+        for(int j=0;j<m;j++)
+        { int temp=prev[j];
+            mini=Math.min(mini,temp);
+        }
+        return mini;
+    }
+}
