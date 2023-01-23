2c9b0658b448a6f89c6e6a41520a66c80622fccf
saumil11-coder <84261342+saumil11-coder@users.noreply.github.com>
2023-01-23 10:46:12 +0530

    Time: 2 ms (99.00%), Space: 50.4 MB (46.88%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        if(n==1 && trust.length==0)
+            return 1;
+        int count[] = new int[n+1];
+        for(int i=0;i<trust.length;i++){
+            count[trust[i][0]]--;
+            count[trust[i][1]]++;
+        }
+        for(int i=1;i<count.length;i++){
+            if(count[i]==n-1)
+                return i;
+        }
+        return -1;
+    }
+}
